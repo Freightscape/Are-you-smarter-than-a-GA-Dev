057961b82b3a0959cd5cafa480301be9a05415da
Freightscape <jordan@freightscape.net>
2022-05-23 12:03:26 -0400

    Lunch Break Pause

--- app.js
+++ app.js
@@ -7,3 +7,18 @@ const contdownDisplay = document.getElementById("countdown");
 const playerStrikes = document.getElementById("strikes");
 
 function questionPlayer() {}
+
+questionPlayer();
+
+const gameQuestions = [
+  {
+    question: "Asked This?",
+    answer: {
+      A: "First Choice",
+      B: "Second Choice",
+      C: "Third Choice",
+      D: "Fourth Choice",
+    },
+    rightAnswer: "B",
+  },
+];

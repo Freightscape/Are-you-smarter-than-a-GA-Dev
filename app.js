802308f89c2d140432eb5f6de71b10e5be052b8e
Freightscape <jordan@freightscape.net>
2022-05-23 11:55:31 -0400

    Step 2 - Question Frameworks Built w/ Time Indicator and Submit Button

---
+++ app.js
@@ -0,0 +1,9 @@
+// Building out the Quiz starting with containers
+//Grabbing my QuizPannel, Submit Button, & Final Score from HTML
+
+const questionsContainer = document.getElementById("Question-Panel");
+const submitButton = document.getElementById("submit");
+const contdownDisplay = document.getElementById("countdown");
+const playerStrikes = document.getElementById("strikes");
+
+function questionPlayer() {}
